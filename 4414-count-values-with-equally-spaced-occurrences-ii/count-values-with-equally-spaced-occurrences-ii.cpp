6938dc6e6890160f72657240ebaac6a6265917a1
adavkarvarad2006 <adavkarvarad2006@gmail.com>
2026-09-12 23:21:03 +0530

    Time: 316 ms (41.63%) | Memory: 318.9 MB (45.67%) - LeetSync

--- 4414-count-values-with-equally-spaced-occurrences-ii/count-values-with-equally-spaced-occurrences-ii.cpp
+++ 4414-count-values-with-equally-spaced-occurrences-ii/count-values-with-equally-spaced-occurrences-ii.cpp
@@ -4,24 +4,24 @@ public:
         unordered_map<int, vector<int>> mp;
         int ans = 0;
 
-        for(int i=0;i<nums.size();i++){
+        for (int i = 0; i < nums.size(); i++) {
             mp[nums[i]].push_back(i);
         }
 
-        for(auto it:mp){
-            if(it.second.size() >= 3){
+        for (auto it : mp) {
+            if (it.second.size() >= 3) {
                 int diff = it.second[1] - it.second[0];
                 bool flag = false;
-                for(int i=2; i<it.second.size(); i++){
-                    if(it.second[i] - it.second[i-1] != diff){
+                for (int i = 2; i < it.second.size(); i++) {
+                    if (it.second[i] - it.second[i - 1] != diff) {
                         flag = true;
                         break;
                     }
                 }
 
-                if(!flag){
-                ans++;
-            }
+                if (!flag) {
+                    ans++;
+                }
             }
         }
 
