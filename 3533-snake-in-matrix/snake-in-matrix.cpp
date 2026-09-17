afcd6f0bb2a4ef49cb489ded56b8579fda2facdc
adavkarvarad2006 <adavkarvarad2006@gmail.com>
2026-09-17 20:12:28 +0530

    Time: 0 ms (100.00%) | Memory: 34.8 MB (77.56%) - LeetSync

---
+++ 3533-snake-in-matrix/snake-in-matrix.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int finalPositionOfSnake(int n, vector<string>& commands) {
+        int i=0, j=0;
+
+        for(int k=0; k<commands.size(); k++){
+            if(commands[k] == "UP"){
+                i--;
+            }
+            else if(commands[k] == "RIGHT"){
+                j++;
+            }
+            else if(commands[k] == "DOWN"){
+                i++;
+            }
+            else{
+                j--;
+            }
+        }
+
+        return (i*n)+j;
+    }
+};
