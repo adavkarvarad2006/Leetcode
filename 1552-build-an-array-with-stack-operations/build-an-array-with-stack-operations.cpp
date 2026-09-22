4f1b671bfa6be609eb7fec7ed21a46a833919471
adavkarvarad2006 <adavkarvarad2006@gmail.com>
2026-09-22 12:43:51 +0530

    Time: 3 ms (6.06%) | Memory: 10.7 MB (56.17%) - LeetSync

--- 1552-build-an-array-with-stack-operations/build-an-array-with-stack-operations.cpp
+++ 1552-build-an-array-with-stack-operations/build-an-array-with-stack-operations.cpp
@@ -2,26 +2,15 @@ class Solution {
 public:
     vector<string> buildArray(vector<int>& target, int n) {
         vector<string> ans;
-        stack<int> s;
-
-        int i=1;
         int j=0;
 
-        while(s.size() != target.size()){
-            s.push(i);
+        for(int i=1; j<target.size(); i++){
             ans.push_back("Push");
 
-            bool flag = false;
-            if(s.top() != target[j]){
-                s.pop();
-                ans.push_back("Pop");
-                flag = true;
-            }
-
-            i++;
-
-            if(!flag)
+            if(i == target[j])
                 j++;
+            else
+                ans.push_back("Pop");
         }
 
         return ans;
